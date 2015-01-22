a42756c6501c145843f1f88e3bfbfab46be57bd7
noogoom <livingformj@ymail.com>
2015-01-22 22:48:20 +0100

    Click öffnet weißes div. wow.
    So Zeugs
    Alles nur sporadisch
    - bei click auf "weiter" kommt man zur Raeumeansicht (pdf) aber nicht
    zurueck :'
    - hässlich hardcoded im html (jQuery funktionierte nicht :()

--- raeume.js
+++ raeume.js
@@ -14,10 +14,25 @@ if (Meteor.isClient) {
       Session.set("counter", Session.get("counter") + 1);
     }
   });*/
+
+  $('.backBtn').click(
+         function () {
+             toggleMenu1();
+         }
+       )
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
   });
+}
+
+//rollt Menu ein/aus
+function toggleMenu1() {
+    //if($('#menu').css('display') == 'none')
+    //    $('#menu').effect('slide', { direction: 'right', mode: 'show' });
+    //else
+    //    $('#menu').effect('slide', { direction: 'left', mode: 'hide' });
 }
