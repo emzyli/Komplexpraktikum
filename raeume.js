f9f3f7742695060a4b8dd5633ac3cf638a3fc461
noogoom <livingformj@ymail.com>
2015-01-23 16:26:47 +0100

    Kleine Änderungen
    jQuery-Code sollte wenn Raeume/Filter angeklickt wird in Abhängigkeit
    davon ob im div '#menu' schon Raeume/Filter steht 'menu' zuklappen und
    andernfalls Raeume/Filter schreiben hineinschreiben. Buggy.

--- raeume.js
+++ raeume.js
@@ -1,38 +1,54 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    // counter starts at 0
+    Session.setDefault("counter", 0);
 
-/*  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+    /*  Template.hello.helpers({
+        counter: function () {
+          return Session.get("counter");
+        }
+      });
+    
+      Template.hello.events({
+        'click button': function () {
+          // increment the counter when button is clicked
+          Session.set("counter", Session.get("counter") + 1);
+        }
+      });*/
+
+    Template.navi.events({
+        'click div#roomBtn': function () {
+            toggleMenuR();
+        },
+        'click div#filterBtn': function () {
+            toggleMenuF();
+        }
+    });
+
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+            // code to run on server at startup
+
+        });
     }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+    //rollt Menu ein/aus
+    function toggleMenuR() {
+        var menuR = $('#menu').text();
+        if (!$('menuR:contains("R&auml;ume")')){
+            $('#menu').effect('slide', { direction: 'left', mode: 'show' });
+            $('#menu').html('<p>R&auml;ume</p>');
+        }
+        else
+            $('#menu').effect('slide', { direction: 'right', mode: 'hide' });
     }
-  });*/
-
-  $('.backBtn').click(
-         function () {
-             toggleMenu1();
-         }
-       )
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-
-  });
-}
-
-//rollt Menu ein/aus
-function toggleMenu1() {
-    if($('#menu').css('display') == 'none')
-        $('#menu').effect('slide', { direction: 'right', mode: 'show' });
-    else
-        $('#menu').effect('slide', { direction: 'left', mode: 'hide' });
+    function toggleMenuF() {
+        var menuF = $('#menu').text();
+        if (!"menuF:contains('Filter')") {
+            $('#menu').effect('slide', { direction: 'left', mode: 'show' });
+            $('#menu').html('<p>Filter</p>');
+        }
+        else
+            $('#menu').effect('slide', { direction: 'right', mode: 'hide' });
+      
+    }
 }
