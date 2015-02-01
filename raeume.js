82ab88f9efc6fc70ad8b9fc750224e7443e80360
noogoom <livingformj@ymail.com>
2015-02-01 23:48:22 +0100

    xml-einbettung funktioniert + zoom-anfang
    3 zoomstufen vorhanden, allerdings ohne positionierung.

--- raeume.js
+++ raeume.js
@@ -10,6 +10,10 @@ if (Meteor.isClient) {
     //Ausklappmenue nicht ausgerollt
     Session.setDefault("toggled", 0);
 
+    //Zoomstufe [0-2]
+    // wird in zoomIn, zoomOut verringert/erhoeht
+    Session.setDefault("zoomLevel", 0);
+
     //Navigationsbuttons auf Karte
     var upBtn =  $('#levelUp');
     upBtn.disabled = true;
@@ -19,19 +23,19 @@ if (Meteor.isClient) {
 
     //Interaktion mit der NaviLeiste
     Template.navi.events({
-        'click div#backBtn': function () {
+        'click div#first': function () {
             changeSVG(0, 2, Session.get("position"));
             toggleMenu(0);
         },
-        'click div#roomBtn': function () {
+        'click div#ort': function () {
             if(Session.get("position"))
                 toggleMenu("ort");
             else  toggleMenu("ort");
         },
-        'click div#filterBtn': function () {
+        'click div#filter': function () {
             toggleMenu("filter");
         },
-        'click div#infoBtn': function () {
+        'click div#info': function () {
             toggleMenu("info");
         }
     });
@@ -57,14 +61,73 @@ if (Meteor.isClient) {
 
     //Interaktion mit dem Inhalt (SVG-Karten und Buttons)
     Template.content.events({
+        //mach es draggable
+        'click div#frameContent': function(){
+            $('#frameContent').mousedown(handle_mousedown($(this)));
+        },
         'click div#levelDown' : function() {
             changeSVG(Session.get("floor"), 1, 1);
         },
         'click div#levelUp' : function() {
             changeSVG(Session.get("floor"), 0, 1);
-        }
-    });    
+        },
+        'click a#in': function () {
+            zoomIn();
+        },
+        'click a#out': function () {
+            zoomOut();
+    }
+    });
 
+    //reinZoomen
+    function zoomIn() {
+        zlevel = Session.get('zoomLevel');
+        if (zlevel == 2) {
+            // ignore
+        }
+        else {
+            if (zlevel == 1) {
+                //weiter geht es nicht, +Link deaktivieren
+                $('#in').addClass('disabled');
+                $('#in').attr('style', 'opacity: 0.1; cursor: default');
+            }
+            if (zlevel == 0) {
+                //es geht nun zurueck, -Link aktivieren
+                $('#out').removeClass('disabled');
+                $('#out').attr('style', 'opacity: 1; cursor: pointer');
+            }
+            zlevel++;
+            Session.set("zoomLevel", zlevel);
+            $('#frameContent').css("background-size", (zlevel + 1) * 100 + '%');
+            $('#frameContent').css("-webkit-transform:", "scale(1.4)");
+            $('#frameContent').css("-moz-transform:", "scale(1.4)");
+        }
+    }
+
+    //rausZoomen
+    function zoomOut() {
+        zlevel = Session.get('zoomLevel');
+        if (zlevel == 0) {
+            // ignore
+        }
+        else {
+            if (zlevel == 1) {
+                //weiter geht es nicht, -Link deaktivieren
+                $('#out').addClass('disabled');
+                $('#out').attr('style', 'opacity: 0.1; cursor: default');
+            }
+            if (zlevel == 2) {
+                //wieder hineinzoomen mgl, +Link aktivieren
+                $('#in').removeClass('disabled');
+                $('#in').attr('style', 'opacity: 1; cursor: pointer');
+            }
+            zlevel--;
+            Session.set("zoomLevel", zlevel);
+            $('#frameContent').css("background-size", (zlevel + 1) * 100 + '%');
+            $('#frameContent').css("-webkit-transform:", "scale(0.4)");
+            $('#frameContent').css("-moz-transform:", "scale(0.4)");
+        }
+    }
 }
     //btnType: Raeume oder Filter
     function toggleMenu(btnType) {
@@ -93,17 +156,16 @@ if (Meteor.isClient) {
             Session.set("toggled", 0);
             $('#menu').effect('slide', { direction: 'left', mode: 'hide' });
             //roten Balken wieder zeigen
-            $('nav').css('border-right', '5px solid #cc0000');
-            $('#'+btnType + ' a div').css('background', "url('icon_"+btnType+".svg') no-repeat");
+            $('nav').css('border-right', '10px solid #cc0000');
+            $('#'+btnType + ' a div').css('background-image', "url('icon_"+btnType+".svg')");
             $('#'+btnType + ' a div').css('background-size', "125px");
         }
     }
     function toggleMenuContent(btnType) {
-
-        //lösche alle momentanen Elemente der Liste
-        $('#menuList').empty();
         var list = $('#menuList');
-
+        //lösche alle momentanen Elemente der Liste
+        list.empty();
+       
         if (btnType == 'filter') toggleMenuFilter(list);
         if (btnType != 'filter') {
             if (!Session.get("position")) {
@@ -113,36 +175,47 @@ if (Meteor.isClient) {
 
             //hole xml-Daten
             var xmlFile;
-            $.get('info.xml', function (data) { xmlFile = data; });
-
-            var entries = xmlFile.getElementsByTagName('raum');
-
-            //fuer jeden Entry Listelemente einfuegen
-            //entry<heading(*), eintrag(*)>            
-            entries.each(function () {
-                //heading falls vorhanden
-                if ($(this).find('heading')) {
-                    liH = $('<li>').addClass('heading');
-                    liH.html($(this).find('heading').text());
-                    list.append(liH);
-                }
-                //id falls vorhanden
-                if ($(this).find('id')) {
-                    liH = $('<li>').addClass('menuel');
-                    liH.html($(this).find('id').text());
-                    list.append(liH);
-                }
-                //eintraege falls vorhanden
-                if ($(this).find('eintrag')) {
-                    liEList = $(this).find('eintrag');
-                    liEList.each(function () {
-                        liE = $('<li>').addClass('menuel');
-                        liE.html($(this).text());
-                        list.append(liE);
-                    });
-                }
+            $(document).ready(function () {
+                $.ajax({
+                    type: "GET",
+                    url: btnType+".xml",
+                    dataType: "xml",
+                    success: fillList
+                });
             });
         }
+    }
+
+//'find' braucht nen hack: http://stackoverflow.com/questions/15776529/jquery-ajax-xml-find-works-in-ie-but-not-chrome
+    function fillList(xmlFile) {
+        var list = $('#menuList');
+        var entries = $(xmlFile).find('entry');
+
+        //fuer jeden Entry Listelemente einfuegen
+        //entry<heading(*), eintrag(*)>            
+        entries.each(function () {
+            //heading falls vorhanden
+            if ($(this).find('heading').length > 0) {
+                liH = $('<li>').addClass('heading');
+                liH.html($(this).find('heading').text());
+                list.append(liH);
+            }
+            //id falls vorhanden
+            if ($(this).find('id').length > 0) {
+                liH = $('<li>').addClass('menuel');
+                liH.html($(this).find('id').text());
+                list.append(liH);
+            }
+            //eintraege falls vorhanden
+            if ($(this).find('eintrag').length > 0) {
+                liEList = $(this).find('eintrag');
+                liEList.each(function () {
+                    liE = $('<li>').addClass('menuel');
+                    liE.html($(this).text());
+                    list.append(liE);
+                });
+            }
+        });
     }
 
     function toggleMenuFilter(list) {
@@ -223,9 +296,10 @@ if (Meteor.isClient) {
         var buttons = $('.levelBtn');
         if(i==1) {
             if (f == 0) {
-                iframe.attr('src', 'ebene-1.svg');
+                iframe.css('background', "url('ebene-1.svg') no-repeat");
                 //floor = -1
                 Session.set("floor", Session.get("floor") - 1);
+                Session.set("zoomLevel", 0);
                 upBtn.disabled =  downBtn.disabled = false;
 
                 tower.css('background', "url('stapel-1.svg') no-repeat");
@@ -233,9 +307,10 @@ if (Meteor.isClient) {
                 up.css('-webkit-filter', 'grayscale(0%)');
                 down.css('-webkit-filter', 'grayscale(0%)');
             } else if (f == -1) {
-                iframe.attr('src', 'ebene-2.svg');
+                iframe.css('background', "url('ebene-2.svg') no-repeat");
                 //floor = -2
                 Session.set("floor", Session.get("floor") - 1);
+                Session.set("zoomLevel", 0);
                 upBtn.disabled = false;
                 downBtn.disabled = true;
 
@@ -246,9 +321,10 @@ if (Meteor.isClient) {
             }
         }else if(i==0){
             if (f ==  -1) {
-                iframe.attr('src', 'Ebene0.svg');
+                iframe.css('background', "url('Ebene0.svg') no-repeat");
                 //floor = 0
                 Session.set("floor", Session.get("floor") + 1);
+                Session.set("zoomLevel", 0);
                 upBtn.disabled = true;
                 downBtn.disabled = false;
 
@@ -258,9 +334,10 @@ if (Meteor.isClient) {
                 downBtn.css('-webkit-filter', 'grayscale(0%)');
 
             } else if (f == -2) {
-                iframe.attr('src', 'ebene-1.svg');
+                iframe.css('background', "url('ebene-1.svg') no-repeat");
                 //floor = -1
                 Session.set("floor", Session.get("floor") + 1);
+                Session.set("zoomLevel", 0);
                 upBtn.disabled = downBtn.disabled = false;
 
                 tower.css('background', "url('stapel-1.svg') no-repeat");
@@ -271,13 +348,39 @@ if (Meteor.isClient) {
         }else{
             if (p){
                 Session.set("position", 0);
-                iframe.attr('src', 'map1.svg');
+                Session.set("zoomLevel", 0);
+                iframe.css('background', "url('map1.svg') no-repeat");
                 buttons.css('visibility', 'hidden');
             }else {
                 Session.set("position", 1);
-                iframe.attr('src', 'Ebene0.svg');
+                Session.set("zoomLevel", 0);
+                iframe.css('background', "url('Ebene0.svg') no-repeat");
                 buttons.css('visibility', 'visible');
             }
+        }
+
+        //http://stackoverflow.com/questions/2424191/how-to-make-a-element-draggable-use-jquery
+        //draggable fkt
+        function handle_mousedown(e){
+            window.my_dragging = {};
+            my_dragging.pageX0 = e.pageX;
+            my_dragging.pageY0 = e.pageY;
+            my_dragging.elem = this;
+            my_dragging.offset0 = $(this).offset();
+            function handle_dragging(e){
+                var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+                var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+                $(my_dragging.elem)
+                .offset({top: top, left: left});
+            }
+            function handle_mouseup(e){
+                $('body')
+                .off('mousemove', handle_dragging)
+                .off('mouseup', handle_mouseup);
+            }
+            $('body')
+            .on('mouseup', handle_mouseup)
+            .on('mousemove', handle_dragging);
         }
 
     }
